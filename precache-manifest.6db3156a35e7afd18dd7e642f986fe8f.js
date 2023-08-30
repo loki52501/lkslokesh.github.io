654538c2d51287d7b6ef7fbaf21858e41de358d1
loki52501 <artcreativity2019@gmail.com>
2023-08-30 20:01:14 +0530

    Updates

---
+++ precache-manifest.6db3156a35e7afd18dd7e642f986fe8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "52035194c6ad14405b35",
+    "url": "/loki52501.github.io/static/css/main.611c4aeb.chunk.css"
+  },
+  {
+    "revision": "52035194c6ad14405b35",
+    "url": "/loki52501.github.io/static/js/main.52035194.chunk.js"
+  },
+  {
+    "revision": "a133a98df0e98e22bc65",
+    "url": "/loki52501.github.io/static/css/1.5773f2bb.chunk.css"
+  },
+  {
+    "revision": "a133a98df0e98e22bc65",
+    "url": "/loki52501.github.io/static/js/1.a133a98d.chunk.js"
+  },
+  {
+    "revision": "03ce2a1962ae4fbcf3c5",
+    "url": "/loki52501.github.io/static/js/runtime~main.03ce2a19.js"
+  },
+  {
+    "revision": "cfdeb1333a1b7772c9ef79f40105b724",
+    "url": "/loki52501.github.io/index.html"
+  }
+];
